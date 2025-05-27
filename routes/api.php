c2bf2ab182366a298bc895dcb4bb7dc8fe2fa4b2
mdr25 <dihyaspider20@gmail.com>
2025-05-27 10:24:32 +0700

    Tugas Pertemuan 6 Laravel - Middleware Users dan Admin

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,35 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Public routes
+Route::get('genres', [GenreController::class, 'index']);
+Route::get('genres/{genre}', [GenreController::class, 'show']);
+Route::get('authors', [AuthorController::class, 'index']);
+Route::get('authors/{author}', [AuthorController::class, 'show']);
+Route::get('books', [BookController::class, 'index']);
+Route::get('books/{book}', [BookController::class, 'show']);
 
-Route::apiResource('genres', GenreController::class);
-Route::apiResource('authors', AuthorController::class);
-Route::apiResource('books', BookController::class);
+// Admin routes
+Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
+    // Genre
+    Route::post('genres', [GenreController::class, 'store']);
+    Route::put('genres/{genre}', [GenreController::class, 'update']);
+    Route::patch('genres/{genre}', [GenreController::class, 'update']);
+    Route::delete('genres/{genre}', [GenreController::class, 'destroy']);
+
+    // Author
+    Route::post('authors', [AuthorController::class, 'store']);
+    Route::put('authors/{author}', [AuthorController::class, 'update']);
+    Route::patch('authors/{author}', [AuthorController::class, 'update']);
+    Route::delete('authors/{author}', [AuthorController::class, 'destroy']);
+
+    // Book
+    Route::post('books', [BookController::class, 'store']);
+    Route::put('books/{book}', [BookController::class, 'update']);
+    Route::patch('books/{book}', [BookController::class, 'update']);
+    Route::delete('books/{book}', [BookController::class, 'destroy']);
+});
